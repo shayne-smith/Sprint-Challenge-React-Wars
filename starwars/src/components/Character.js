36e9b0551b70808d5da264238086cac0deaf43db
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-17 12:04:08 -0500

    wip

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CharacterCard = styled.div`
-    /* src: ${props => props.src ? props.image : 'inherit'}; */
     background-color: white;
     width: 320px;
     height: 400px;
